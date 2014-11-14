8bb02ba463e0e7bbb655054c72fb21b7d632517f
Wei Peng <write.to.peng.wei@gmail.com>
2014-11-13 20:34:06 -0500

    update lein deps

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,22 +1,23 @@
 {
 
  :user {
-
+        
         :plugins [[lein-droid "0.2.3"]
                   [lein-cljsbuild "1.0.3"]
-                  [lein-marginalia "0.7.1"]
+                  [lein-marginalia "0.8.0"]
+                  ;;[lein-voom "0.1.0-20140716_032004-g85e4c9b"]
                   [lein-ancient "0.5.5"]
                   [lein-cprint "1.0.0"]
-                  [lein-pprint "1.1.1"]
+                  [lein-pprint "1.1.2"]
                   [lein-shell "0.4.0"]
                   [lein-localrepo "0.5.3"]
-                  [lein-try "0.4.1"]
+                  [lein-try "0.4.3"]
                   [cider/cider-nrepl "0.8.0-SNAPSHOT"]]
 
         :dependencies [
                        [slamhound "1.5.5"]
-                       [compliment "0.1.3"]
-                       [alembic "0.2.1"]
+                       [compliment "0.2.0"]
+                       [alembic "0.3.2"]
                        ]
 
         :aliases {
