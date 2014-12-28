9d42d66d0610c8e0d65040b9c45c8e8b149303b7
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2014-12-28 14:16:18 -0500

    lein profiles: add lein-javadoc and reorganize plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,18 +2,26 @@
 
  :user {
         
-        :plugins [[lein-droid "0.2.3"]
+        :plugins [
+                  ;; for Cider (Clojure IDE and Repl)
+                  [cider/cider-nrepl "0.8.1"]
+                  [refactor-nrepl "0.2.2"]
+
+                  ;; other Clojure targets
                   [lein-cljsbuild "1.0.3"]
+                  [lein-droid "0.2.3"]
+
+                  ;; misc plugins
+                  [lein-localrepo "0.5.3"]
                   [lein-marginalia "0.8.0"]
-                  ;;[lein-voom "0.1.0-20140716_032004-g85e4c9b"]
+                  [lein-javadoc "0.1.1"]
                   [lein-ancient "0.5.5"]
                   [lein-cprint "1.0.0"]
                   [lein-pprint "1.1.2"]
                   [lein-shell "0.4.0"]
-                  [lein-localrepo "0.5.3"]
                   [lein-try "0.4.3"]
-                  [cider/cider-nrepl "0.8.1"]
-                  [refactor-nrepl "0.2.2"]]
+                  
+                  ]
 
         :dependencies [
                        [slamhound "1.5.5"]
